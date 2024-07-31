1148992f6efbc44cb8b62d8fd8049af7bc18ea1d
Marcelo Silva <marcelo_810@hotmail.com>
2024-07-30 22:40:45 -0300

    💄 new styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react";
-
+import "./globals.css";
 const array = [
 {
   title: 'Teste',
@@ -15,13 +15,15 @@ const array = [
 export default function Home() {
 
   return (
-    <main className="flex gap-8">
+    <main className="flex justify-center items-center gap-8 bg-black text-white h-lvh w-full">
       <div className="flex gap-8">
-      {array.map((item, index) => (
-        <a key={index} href={item.title}>{item.title}</a>
-      ))}
+        {array.map((item, index) => (
+          <a key={index} href={item.title}>{item.title}</a>
+        ))}
       </div>
-      
+      <div>
+        <button className="bg-slate-600 p-5 rounded-3xl" >Teste</button>
+      </div>
     </main>
   );
 }
