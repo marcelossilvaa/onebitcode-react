5da5925fe242d7701a50ad5ce76b121fc7ed1a21
Marcelo Silva <marcelo_810@hotmail.com>
2024-08-01 22:54:47 -0300

    ✅ create generate passowrds

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
-import "./globals.css";
+import useCounter from "./hooks/useCounter";
+
 const array = [
 {
   title: 'Teste',
@@ -12,18 +13,40 @@ const array = [
 },
 ]
 
+const buttonStyles = "bg-zinc-900 p-4 rounded-lg hover:bg-zinc-800 transition-colors duration-500 shadow-xl active:focus:duration-200 active:focus:bg-zinc-600"
+
 export default function Home() {
+  const counter = useCounter()
+
+  const [password, setPassowrd] = useState('')
+  const [copyText, setCopyText] = useState('Copiar')
+  
+  function generate(){
+    const characters = "'123456789-=qwertyuiopasdfghjklzxcvbnm"
+    let length = 12
+    let newPassword = ""
+    for (let i = 0; i < length; i++){
+      const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+    }
+    setPassowrd(newPassword)
+    setCopyText("Copiar")
+  }
+
+  function copyToClipBoard(){
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+
 
   return (
-    <main className="flex justify-center items-center gap-8 bg-black text-white h-lvh w-full">
-      <div className="flex gap-8">
-        {array.map((item, index) => (
-          <a key={index} href={item.title}>{item.title}</a>
-        ))}
+    <main className="flex justify-center flex-col items-center gap-8 bg-zinc-800 text-white h-lvh w-full">
+      <h1 className="text-2xl">Gerador de senhas</h1>
+      <div className="flex gap-8 ">
+        <button onClick={generate} className={buttonStyles}>Gerar!</button>
+        <button className={buttonStyles} onClick={copyToClipBoard}>{copyText}</button>
       </div>
-      <div>
-        <button className="bg-slate-600 p-5 rounded-3xl" >Teste</button>
-      </div>
+      <div>{password}</div>
     </main>
   );
 }
