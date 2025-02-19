844da67cc78cb5db15bc55568f2c30fcaf7f2ad8
Marcelo Silva <marcelo_810@hotmail.com>
2025-02-18 23:25:53 -0300

    chore: Update dependencies and refactor NewGameForm component

---
+++ app/components/NewGameForm.tsx
@@ -0,0 +1,69 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface GameCardProps {
+  title: string;
+  cover: string;
+  onRemove: () => void;
+}
+
+export default function GameCard({title, cover, onRemove}: GameCardProps) {
+  return(
+    <div className="mb-4 flex gap-5 bg-neutral-700 p-4 rounded-2xl">
+      <img src={cover} alt={title} className="w-32 h-32 object-cover rounded-2xl" />
+      <div className="flex flex-col gap-4 justify-around">
+        <h2>{title}</h2>
+        <button className="buttonStyles !py-2" onClick={onRemove}>Romover Jogo</button>
+      </div>
+    </div>
+  )
+}
+
+interface NewGameForm {
+  addGame: (game: { title: string; cover: string }) => void;
+}
+
+
+export function NewGameForm({ addGame }: NewGameForm) {
+  const [title, setTitle] = useState<string>("")
+  const [cover, setCover] = useState<string>("")
+
+  const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+    addGame({title, cover})
+    console.log({title, cover})
+    setTitle("")
+    setCover("")
+  }
+
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const handleCoverChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCover(e.target.value)
+  }
+
+  return (
+    <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+      <NewGameInput type="title" label="Título" value={title} onChange={handleTitleChange} />
+      <NewGameInput type="cover" label="Capa" value={cover} onChange={handleCoverChange} />
+      <button type="submit" className="buttonStyles">Adicionar à biblioteca</button>
+    </form>
+  )
+}
+
+function NewGameInput ({type, label, value, onChange}: {type:string ,label:string, value: string; onChange: (e: ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={type}>{label}:</label>
+      <input 
+        type="text" 
+        id={type} 
+        name={type} 
+        className='buttonStyles'
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,19 +1,28 @@
 'use client'
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import GameCard, { NewGameForm } from "../components/NewGameForm";
+interface Game {
+  id: number;
+  title: string;
+  cover: string;
+}
 
 export default function Game() {
-  const [title, setTitle] = useState("")
-  const [cover, setCover] = useState("")
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedGames = localStorage.getItem("obc-game-lib")
+      if (storedGames) {
+        setGames(JSON.parse(storedGames) as Game[])
+      }
+    }
+  }, [])
 
   const addGame = ({title, cover}: { title: string; cover: string }) => {
     const id  = Math.floor(Math.random() * 1000000)
-    const game = {id, title, cover}
-    setGames((state: any) => {
+    const game: Game = { id, title, cover }
+    setGames((state) => {
       const newState = [...state, game]
       localStorage.setItem("obc-game-lib", JSON.stringify(newState))
       return newState
@@ -21,61 +30,27 @@ export default function Game() {
   )}
   
   const removeGame = (id: number) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
+    setGames((state) => {
+      const newState = state.filter((game: any) => game.id !== id)
       localStorage.setItem("obc-game-lib", JSON.stringify(newState))
       return newState
     })
   }
 
-  const handleSubmit = (ev: any) => {
-    ev.preventDefault()
-    addGame({title, cover})
-    console.log({title, cover})
-    setTitle("")
-    setCover("")
-  }
-
-  
   return (
-    <div className="flex justify-center items-center h-lvh bg-zinc-800 text-white">
-      <div className="">
+    <div className="flex justify-center items-center bg-zinc-800 text-white">
+      <div className="my-10">
         <h1 className="text-2xl font-bold mb-2">Biblioteca de Jogos</h1>
-        <form action="" className="flex flex-col gap-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="title">Título:</label>
-            <input 
-              type="text" 
-              id="title" 
-              name="title" 
-              className='buttonStyles' 
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="cover">Capa: </label>
-            <input 
-              type="text" 
-              id="cover" 
-              name="cover" 
-              className='buttonStyles'
-              value={cover}
-              onChange={(e) => setCover(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="buttonStyles">Adicionar à biblioteca</button>
-        </form>
+        <NewGameForm addGame={addGame} />
         <div className="my-4">
           {
-            games.map(game => (
-              <div className="mb-4 flex gap-5 bg-neutral-700 p-4 rounded-2xl" key={game.id}>
-                  <img src={game.cover} alt={game.title} className="w-32 h-32 object-cover rounded-2xl" />
-                  <div className="flex flex-col gap-4 justify-around">
-                    <h2>{game.title}</h2>
-                    <button className="buttonStyles !py-2" onClick={() => removeGame(game.id)}>Romover Jogo</button>
-                  </div>
-              </div>
+            games.map((game) => (
+              <GameCard 
+                key={game.id}
+                title={game.title}
+                cover={game.cover}
+                onRemove={() => removeGame(game.id)}
+              />
             ))
           }
         </div>
