99914d2213d92996125df0d33c9228d5d64f03a0
Marcelo Silva <marcelo_810@hotmail.com>
2025-03-11 23:18:08 -0300

    chore: condition any game

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,9 +1,9 @@
-'use client'
-import GameCard, { NewGameForm } from "../components/NewGameForm";
-import useGameCollection from "../hooks/useGameCollection";
+'use client';
+import GameCard, { NewGameForm } from '../components/NewGameForm';
+import useGameCollection from '../hooks/useGameCollection';
 
 export default function Game() {
-  const { games, addGame, removeGame } = useGameCollection()
+  const { games, addGame, removeGame } = useGameCollection();
 
   return (
     <div className="flex justify-center items-center bg-zinc-800 text-white h-dvh">
@@ -11,16 +11,18 @@ export default function Game() {
         <h1 className="text-2xl font-bold mb-2">Biblioteca de Jogos</h1>
         <NewGameForm addGame={addGame} />
         <div className="my-4 flex flex-wrap gap-[4%]">
-          {
+          {games.length === 0 && <p>Nenhum jogo adicionado</p> ? (
+            <h2 className="text-xl font-bold my-7 text-center w-full">Nenhum jogo adicionado</h2>
+          ) : (
             games.map((game) => (
-              <GameCard 
+              <GameCard
                 key={game.id}
                 title={game.title}
                 cover={game.cover}
                 onRemove={() => removeGame(game.id)}
               />
             ))
-          }
+          )}
         </div>
       </div>
     </div>
