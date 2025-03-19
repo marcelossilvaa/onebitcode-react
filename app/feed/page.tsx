56880559fbab28e0b5958652bd946261d4362d62
Marcelo Silva <marcelo_810@hotmail.com>
2025-03-18 22:55:17 -0300

    ✨ Enhance comments feature in Feed component: added name and date fields to form, updated state management, and improved comment display.

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,8 +2,8 @@
 import React, { useState } from "react"
 
 export default function Feed(){
-  const [formData, setFormData] = useState({email: "", comment: ""})
-  const [comments, setComments] = useState<{email: string, comment: string}[]>([]);
+  const [formData, setFormData] = useState({email: "", comment: "", date : "", name: ""});
+  const [comments, setComments] = useState<{email: string, comment: string, date: string, name: string}[]>([]);
 
   const handleChange = (e: any) => {
     const {id, value} = e.target;
@@ -12,8 +12,11 @@ export default function Feed(){
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    setComments(prevComments => [...prevComments, formData]);
-    setFormData({ email: "", comment: "" }); 
+    const date = new Date(Date.now()).toLocaleString();
+    const commentWithDate = {...formData, date}
+    setComments(prevComments => [...prevComments, commentWithDate]);
+    setFormData(prevData => ({ ...prevData, date }));
+    setFormData({ email: "", comment: "", date: date, name: ""}); 
   };
 
   return (
@@ -22,21 +25,28 @@ export default function Feed(){
         <h3 className="text-2xl">Seção de Comentários</h3>
         <form className="flex flex-col gap-4 w-full max-w-3xl" onSubmit={handleSubmit}>
           <div className="flex flex-col">
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" className="text-black p-4" value={formData.name} onChange={handleChange} />
+          </div>
+          <div className="flex flex-col">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" className="text-black" value={formData.email} onChange={handleChange} />
+            <input type="email" id="email" className="text-black p-4" value={formData.email} onChange={handleChange} />
           </div>
           <div className="flex flex-col">
             <label htmlFor="comment">Comentário</label>
-            <textarea id="comment" className="text-black" value={formData.comment} onChange={handleChange}></textarea>
+            <textarea id="comment" className="text-black  p-4" rows={6} cols={30} value={formData.comment} onChange={handleChange}></textarea>
           </div>
           <button className="buttonStyles" type="submit">Enviar</button>
         </form>
       </div>
       <div className="p-8">
         <h3 className="text-2xl">Comentários Recentes</h3>
-        {comments.map(({email, comment}) => (
+        {comments.map(({email, comment, date, name}) => (
           <div key={email} className="flex flex-col gap-2 items-start">
-            <p><strong>{email}</strong>: {comment}</p>
+            <p><strong>{name}</strong></p>
+            <p><strong>{email}</strong></p>
+            <p>Data: {date}</p>
+            <p className="te">-{comment}</p>
           </div>
         ))}
       </div>
