d783d135219ea377d66236ba03456719f4f442ea
Marcelo Silva <marcelo_810@hotmail.com>
2025-03-19 23:50:50 -0300

    ✨ Feed component: added age field to form, updated state management to include id and age, and improved comment display with age information.

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Feed(){
-  const [formData, setFormData] = useState({email: "", comment: "", date : "", name: ""});
-  const [comments, setComments] = useState<{email: string, comment: string, date: string, name: string}[]>([]);
+  const [formData, setFormData] = useState({id: 0, email: "", comment: "", date : "", name: "", age: 0});
+  const [comments, setComments] = useState<{id: number, email: string, comment: string, date: string, name: string, age: number}[]>([]);
 
   const handleChange = (e: any) => {
     const {id, value} = e.target;
@@ -13,11 +13,17 @@ export default function Feed(){
   const handleSubmit = (e: any) => {
     e.preventDefault();
     const date = new Date(Date.now()).toLocaleString();
-    const commentWithDate = {...formData, date}
+    const id = Math.floor(Math.random() * 1000);
+    const commentWithDate = {...formData, id, date}
     setComments(prevComments => [...prevComments, commentWithDate]);
+    
     setFormData(prevData => ({ ...prevData, date }));
-    setFormData({ email: "", comment: "", date: date, name: ""}); 
+    setFormData({ id:0, email: "", comment: "", date: date, name: "", age: 0}); 
   };
+
+  useEffect(() => {
+    console.log("📌 ~ handleSubmit ~ comments", comments)
+  }, [comments])
 
   return (
     <main className="m-10">
@@ -27,6 +33,10 @@ export default function Feed(){
           <div className="flex flex-col">
             <label htmlFor="name">Nome</label>
             <input type="text" id="name" className="bg-zinc-900  p-4" value={formData.name} onChange={handleChange} />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="age">Idade</label>
+            <input type="text" id="age" className="bg-zinc-900  p-4" value={formData.age} onChange={handleChange} />
           </div>
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
@@ -41,12 +51,13 @@ export default function Feed(){
       </div>
       <div className="p-8">
         <h3 className="text-2xl">Comentários Recentes</h3>
-        {comments.map(({email, comment, date, name}) => (
-          <div key={email} className="flex flex-col gap-2 items-start">
-            <p><strong>{name}</strong></p>
+        {comments.map(({email, comment, date, name, age}) => (
+          <div key={email} className="flex flex-col gap-2 items-start mb-10">
+            <p><strong>{name} - {age} anos</strong></p>
             <p><strong>{email}</strong></p>
             <p>Data: {date}</p>
             <p className="te">-{comment}</p>
+            <hr className="h-[2px] w-full bg-black"/>
           </div>
         ))}
       </div>
